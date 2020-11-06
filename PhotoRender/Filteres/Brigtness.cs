ba84da3475ee7f8dec4190c8016a2a8ca0cd0f63
alexzonic <smailik072001@mail.ru>
2020-11-06 23:50:02 +0400

    добавлены классы яркости и контрастности в купе с их логикой

--- PhotoRender/Filteres/Brigtness.cs
+++ PhotoRender/Filteres/Brigtness.cs
@@ -6,8 +6,32 @@ using System.Threading.Tasks;
 
 namespace PhotoRender.Filteres
 {
-    class Brigtness
-    {
+    public static class Brigtness
+    { 
+        public static uint ChangeBrigtness(uint point, int position, int maxValue)
+        {
+            int red;
+            int green;
+            int blue;
 
+            var percent = (100 / maxValue) * position; //кол-во процентов
+
+            red = (int)(((point & 0x00FF0000) >> 16) + percent * 128 / 100);
+            green = (int)(((point & 0x0000FF00) >> 8) + percent * 128 / 100);
+            blue = (int)((point & 0x000000FF) + percent * 128 / 100);
+
+            if (red < 0) red = 0;
+            else if (red > 255) red = 255;
+            
+            if (green < 0) green = 0;
+            else if (green > 255) green = 255;
+            
+            if (blue < 0) blue = 0;
+            else if (blue > 255) blue = 255;
+
+            point = 0xFF000000 | ((uint)red << 16) | ((uint)green << 8) | ((uint)blue);
+
+            return point;
+        }
     }
 }
