f9d7649cd10dd3a8d680734d8b244e8018146bf5
alexzonic <smailik072001@mail.ru>
2020-11-06 20:11:39 +0400

    Класс конвертации в формат .bmp

---
+++ PhotoRender/Convert.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+
+
+namespace PhotoRender
+{
+    class Convert
+    {
+        private const int ResizeRate = 2;
+
+        public static Bitmap ToBitmap(int width, int height, Func<int, int, Color> getPixelColor)
+        {
+            var bmp = new Bitmap(ResizeRate * width, ResizeRate * height);
+            using (var g = Graphics.FromImage(bmp))
+            {
+                for (var x = 0; x < width; x++)
+                    for (var y = 0; y < height; y++)
+                        g.FillRectangle(new SolidBrush(getPixelColor(x, y)),
+                            ResizeRate * x,
+                            ResizeRate * y,
+                            ResizeRate,
+                            ResizeRate
+                        );
+            }
+
+            return bmp;
+        }
+
+        public static Bitmap ConvertToToBitmapBitmap(Pixel[,] array)
+        {
+            return ToBitmap(array.GetLength(0), array.GetLength(1),
+                (x, y) => Color.FromArgb(array[x, y].R, array[x, y].G, array[x, y].B));
+        }
+
+        public static Bitmap ToBitmap(double[,] array)
+        {
+            return ToBitmap(array.GetLength(0), array.GetLength(1), (x, y) =>
+            {
+                var gray = (int)(255 * array[x, y]);
+                gray = Math.Min(gray, 255);
+                gray = Math.Max(gray, 0);
+                return Color.FromArgb(gray, gray, gray);
+            });
+        }
+    }
+}
--- PhotoRender/Pixel.cs
+++ PhotoRender/Pixel.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Drawing;
-using System.Windows.Media;
+﻿using System.Windows.Media;
 
 namespace PhotoRender
 {
